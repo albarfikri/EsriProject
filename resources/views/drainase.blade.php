9fa29fa48c375fd662ce8f6e5e0f24b49c793d98
ArdyJunata <ardyjunata53@gmail.com>
2020-12-19 01:27:14 +0700

    adding modal drainase

--- resources/views/drainase.blade.php
+++ resources/views/drainase.blade.php
@@ -13,6 +13,9 @@
             <!-- Card header -->
             <div class="card-header border-0">
               <h3 class="mb-0">Light table</h3>
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Tambah Data
+</button>
             </div>
             <!-- Light table -->
             <div class="table-responsive">
@@ -161,3 +164,117 @@
       </div>
 @endsection
       
+
+<div class="row">
+  <div class="col-md-4">
+      <div class="modal fade" id="modal-default" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
+    <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+        <div class="modal-content">
+        	
+            <div class="modal-header">
+                <h6 class="modal-title" id="modal-title-default">Tambah Data Petugas</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            
+            <div class="modal-body">
+            	
+            <form role="form">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group mb-3">
+                      <div class="input-group input-group-merge input-group-alternative">
+                          <div class="input-group-prepend">
+                              <span class="input-group-text"><i class="ni ni-single-02"></i></span>
+                          </div>
+                          <input class="form-control" placeholder="Nama Petugas" type="text">
+                      </div>
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <div class="form-group mb-3">
+                      <div class="input-group input-group-merge input-group-alternative">
+                          <div class="input-group-prepend">
+                              <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                          </div>
+                          <input class="form-control" placeholder="Email" type="email">
+                      </div>
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="Kata Sandi" type="password">
+                    </div>
+                  </div>
+                  
+              </div>
+              <div class="col-md-6">
+                  <div class="form-group mb-3">
+                      <div class="input-group input-group-merge input-group-alternative">
+                          <div class="input-group-prepend">
+                              <span class="input-group-text"><i class="ni ni-trophy"></i></span>
+                          </div>
+                          <input class="form-control" placeholder="Posisi" type="text">
+                      </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-mobile-button"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="No Hp" type="text">
+                    </div>
+                  </div>
+                  
+              </div>
+              <div class="col-md-6">
+                  <div class="form-group mb-3">
+                      <div class="input-group input-group-merge input-group-alternative">
+                          <div class="input-group-prepend">
+                              <span class="input-group-text"><i class="ni ni-map-big"></i></span>
+                          </div>
+                          <input class="form-control" placeholder="Tempat Lahir" type="text">
+                      </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <div class="input-group input-group-merge input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-calendar-grid-58"></i></span>
+                        </div>
+                        <input class="form-control" type="date" id="example-date-input">
+                    </div>
+                  </div>
+                  
+              </div>
+              <div class="col-md-12">
+                      <div class="text-center">
+                          <button type="button" class="btn btn-primary my-4">Kirim</button>
+                      </div>
+              </div>
+            
+            
+        </form>
+                
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+  </div>
+
+  </div>
+      
+</div>
+
+  </div>
+</div>
